3b832bf9afd65d6135fd671cd83057af5a51ec65
jstran9 <wptran58@gmail.com>
2019-06-24 18:16:40 -0700

    displaying messages after adding a lead and deleting one. also calling our errors reducer when we have error(s) when adding a lead.

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
+import { createMessage } from "./messages";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from "./types";
 
 // GET LEADS
 export const getLeads = () => dispatch => {
@@ -19,6 +20,11 @@ export const deleteLead = id => dispatch => {
   axios
     .delete(`/api/leads/${id}/`)
     .then(response => {
+      dispatch(
+        createMessage({
+          deleteLead: "Lead Deleted"
+        })
+      );
       dispatch({
         type: DELETE_LEAD,
         payload: id
@@ -32,10 +38,24 @@ export const addLead = lead => dispatch => {
   axios
     .post("api/leads/", lead)
     .then(response => {
+      dispatch(
+        createMessage({
+          addLead: "Lead Added"
+        })
+      );
       dispatch({
         type: ADD_LEAD,
         payload: response.data
       });
     })
-    .catch(error => console.log(error));
+    .catch(error => {
+      const errors = {
+        msg: error.response.data,
+        status: error.response.status
+      };
+      dispatch({
+        type: GET_ERRORS,
+        payload: errors
+      });
+    });
 };
