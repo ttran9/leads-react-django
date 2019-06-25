8afe2e81d11919afd184c6d0b9c4ffdf2f7cfc37
jstran9 <wptran58@gmail.com>
2019-06-24 18:16:59 -0700

    creating the get errors action and create message action.

--- leadmanager/frontend/src/actions/types.js
+++ leadmanager/frontend/src/actions/types.js
@@ -1,3 +1,9 @@
 export const GET_LEADS = "GET_LEADS";
 export const DELETE_LEAD = "DELETE_LEAD";
 export const ADD_LEAD = "ADD_LEAD";
+
+// error actions
+export const GET_ERRORS = "GET_ERRORS";
+
+// message actions
+export const CREATE_MESSAGE = "CREATE_MESSAGE";
