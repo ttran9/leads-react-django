e144e96f99754014632e8d0731b9ee8a811af114
jstran9 <wptran58@gmail.com>
2019-06-25 18:45:56 -0700

    adding the token to the retrieve, add, and delete functions from leads.

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { createMessage, returnErrors } from "./messages";
 import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
+import { tokenConfig } from "./auth";
 
 // GET LEADS
-export const getLeads = () => dispatch => {
+export const getLeads = () => (dispatch, getState) => {
   axios
-    .get("/api/leads/")
+    .get("/api/leads/", tokenConfig(getState))
     .then(response => {
       dispatch({
         type: GET_LEADS,
@@ -18,9 +19,9 @@ export const getLeads = () => dispatch => {
 };
 
 // DELETE LEAD
-export const deleteLead = id => dispatch => {
+export const deleteLead = id => (dispatch, getState) => {
   axios
-    .delete(`/api/leads/${id}/`)
+    .delete(`/api/leads/${id}/`, tokenConfig(getState))
     .then(response => {
       dispatch(
         createMessage({
@@ -36,9 +37,9 @@ export const deleteLead = id => dispatch => {
 };
 
 // ADD LEAD
-export const addLead = lead => dispatch => {
+export const addLead = lead => (dispatch, getState) => {
   axios
-    .post("api/leads/", lead)
+    .post("api/leads/", lead, tokenConfig(getState))
     .then(response => {
       dispatch(
         createMessage({
